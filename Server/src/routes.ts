2f4d212240d07d232c73d19ea53171ec0a7f59c3
Felipe Yui <felipe_m87@yahoo.com.br>
2020-06-07 00:50:17 +0930

    App

---
+++ Server/src/routes.ts
@@ -0,0 +1,40 @@
+import express from 'express';
+import multer from 'multer';
+import { celebrate, Joi } from 'celebrate';
+import multerConfig from './config/multer'
+
+import PointsController from  './controllers/pointsControllers';
+import ItemsController from './controllers/itemsControllers';
+
+const routes = express.Router();
+const upload = multer(multerConfig)
+
+const pointsControllers = new PointsController();
+const itemsControllers = new ItemsController();
+
+routes.get('/items', itemsControllers.index);
+routes.get('/points', pointsControllers.index);
+routes.get('/points/:id', pointsControllers.show);
+
+routes.post(
+  '/points', 
+  upload.single('image'),
+  celebrate({
+    body:Joi.object().keys({
+      name: Joi.string().required(),
+      email: Joi.string().required().email(),
+      whatsapp: Joi.string().required(),
+      latitude: Joi.number().required(),
+      longitude: Joi.number().required(),
+      city: Joi.string().required(),
+      uf: Joi.string().required().max(2),
+      items: Joi.string().required()
+    })
+  },{
+    abortEarly: false
+  }), 
+  pointsControllers.create
+);
+
+
+export default routes;
